89552e254e98b076abd7eafe50f159fd20733003
EvilaMany <lineman98@yandex.ru>
2018-09-11 19:55:54 +0300

    fourth task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+	var parts = equation.split(' ')
+	var n1 = parseInt(parts[0]), n2 = parseInt(parts[3]+parts[4]), n3 = parseInt(parts[7] + parts[8])
+
+	var d = n2**2 - (4 * n1 * n3)
+
+	var x1 = Math.round((-1 * n2 - Math.sqrt(d)) / (2 * n1))
+	var x2 = Math.round((-1 * n2 + Math.sqrt(d)) / (2 * n1))
+
+	return [x1,x2].sort(function(a,b){return a-b})
 }
