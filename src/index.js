69e0fc80b51f54ad011ac3d48ca1f3589896389a
EvilaMany <lineman98@yandex.ru>
2018-09-28 06:54:10 +0300

    change to es6

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 module.exports = function solveEquation(equation) {
-	var parts = equation.split(' ')
-	var n1 = parseInt(parts[0]), n2 = parseInt(parts[3]+parts[4]), n3 = parseInt(parts[7] + parts[8])
+	let parts = equation.split(' ')
+	let n1 = parseInt(parts[0]), 
+		n2 = parseInt(parts[3]+parts[4]), 
+		n3 = parseInt(parts[7] + parts[8])
 
-	var d = n2**2 - (4 * n1 * n3)
+	let d = n2**2 - (4 * n1 * n3)
 
-	var x1 = Math.round((-1 * n2 - Math.sqrt(d)) / (2 * n1))
-	var x2 = Math.round((-1 * n2 + Math.sqrt(d)) / (2 * n1))
+	let x1 = Math.round((-1 * n2 - Math.sqrt(d)) / (2 * n1))
+	let x2 = Math.round((-1 * n2 + Math.sqrt(d)) / (2 * n1))
 
 	return [x1,x2].sort(function(a,b){return a-b})
 }
